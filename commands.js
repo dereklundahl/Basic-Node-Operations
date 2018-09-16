16d87ad1b92dc64092d0bde63e731c23dbf1c4bf
Derek Lundahl <dereklundahl@yahoo.com>
2018-09-16 12:07:31 -0600

    Completed checkpoint basic node operations

---
+++ commands.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+
+function done(output) {
+  process.stdout.write(output);
+  process.stdout.write('\nprompt > ');
+}
+
+function evaluateCmd(userInput) {
+
+  const userInputArray = userInput.split(" ");
+  const command = userInputArray[0];
+
+  switch (command) {
+    case "echo":
+      commandLibrary.echo(userInputArray.slice(1).join(" "));
+      break;
+
+    case "cat":
+      commandLibrary.cat(userInputArray.slice(1));
+      break;
+
+    case "head":
+      commandLibrary.head(userInputArray.slice(1));
+      break;
+
+    case "tail":
+      commandLibrary.tail(userInputArray.slice(1));
+      break;
+
+    default:
+      console.log(`${command} is not a valid command`);
+      break;
+  }
+}
+
+const commandLibrary = {
+  "echo": function(userInput) {
+    done(userInput);
+  },
+
+  "cat": function(fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      done(data);
+    });
+  },
+
+    "head": function(fullPath) {
+      const fileName = fullPath[0];
+      fs.readFile(fileName, (err, data) => {
+        var truncatedData = data.slice(0, 100);
+        if (err) throw err;
+        done(truncatedData);
+      });
+    },
+
+    "tail": function(fullPath) {
+      const fileName = fullPath[0];
+      fs.readFile(fileName, (err, data) => {
+        var truncatedData = data.slice(-100);
+        if (err) throw err;
+        done(truncatedData);
+      });
+    }
+
+
+  };
+
+
+module.exports.commandLibrary = commandLibrary;
+module.exports.evaluateCmd = evaluateCmd;
